94ec785f3f4015ac345ebb318cfbe7bda63b02f8
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-17 15:26:08 +0200

    snack2 TOOLS

--- snack2/script.js
+++ snack2/script.js
@@ -1,1 +1,20 @@
 console.log("its works");
+
+/* 
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
+
+/* 
+TOOLS:
+-creaare un array di oggetti per le squadre di calcio
+-ogni oggetto deve avere nome, punti fatti e falli subiti
+-prompt con il nome da compliare il resto set a 0
+-stampare l'array per verificare se è corretto
+- generare numeri random per punti fatti e subiti
+-creare un array che contenga solo nome e falli subiti
+-stampare il risultate in console
+*/
