30c688ea9d370299417b08b4e2f8dcb8e319515f
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-17 14:09:49 +0200

    create ad array object with different object and insert name and weight

--- snack1/script.js
+++ snack1/script.js
@@ -15,3 +15,21 @@ TOOLS:
 -trovare la bici con il peso minore
 -stampare il risultato nell html con il DOM
 */
+
+//crare un array di oggetti 
+const biciclette = [
+  //ogni bicicletta deve avere un nome e un peso
+  { nome: "bici rossa", peso: 10 },
+  { nome: "bici bianca", peso: 8 },
+  { nome: "bici rosa", peso: 12 },
+  { nome: "bici nera", peso: 7 },
+  { nome: "bici lilla", peso: 20 },
+  { nome: "bici blu", peso: 14 },
+];
+
+// stampare l'array per verificare sia corretto
+
+// trovare la bici con il peso minore
+
+// stampare il risultato in html
+
