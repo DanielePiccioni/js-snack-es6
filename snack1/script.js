61bb666f236d0126d236ec1a3adeadc72fadbeb2
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-17 13:40:22 +0200

    snack1 TOOLS

--- snack1/script.js
+++ snack1/script.js
@@ -1,1 +1,17 @@
 console.log("its works");
+
+/* 
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+*/
+
+/* 
+TOOLS:
+-crare un array di oggetti (biciclette)
+-ogni bicicletta deve avere un nome e un peso proprio
+-stampare l'arrey per verificare che sia corretto
+-trovare la bici con il peso minore
+-stampare il risultato nell html con il DOM
+*/
